59f5d4379ab921fca0a5d7c25853d38631e51f87
Зайцев Илья <ilyazaytsev2017@bk.ru>
2022-05-21 02:58:26 +0700

    RGR5

---
+++ RGR/RGR/Models/ResultDriver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RGR
+{
+    public partial class ResultDriver
+    {
+        public string PosId { get; set; } = null!;
+        public long? NumDriver { get; set; }
+        public string? NameTrack { get; set; }
+        public string? Time { get; set; }
+        public string? Interval { get; set; }
+        public string? KpH { get; set; }
+        public string? Best { get; set; }
+        public long? Laps { get; set; }
+
+        public virtual Track? NameTrackNavigation { get; set; }
+        public virtual Driver? NumDriverNavigation { get; set; }
+    }
+}
---
+++ RGR/RGR/Models/Track.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RGR
+{
+    public partial class Track
+    {
+        public Track()
+        {
+            ResultDrivers = new HashSet<ResultDriver>();
+            Teams = new HashSet<Team>();
+        }
+
+        public string NameId { get; set; } = null!;
+        public long? Year { get; set; }
+        public long? Lap { get; set; }
+
+        public virtual Championship? YearNavigation { get; set; }
+        public virtual ICollection<ResultDriver> ResultDrivers { get; set; }
+        public virtual ICollection<Team> Teams { get; set; }
+    }
+}
